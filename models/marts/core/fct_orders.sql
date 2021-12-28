2f6c8cb111d69355c0d4934b9aa423707610ee53
Anil Kulkarni <anil77k@gmail.com>
2021-12-28 06:41:15 +0000

    exemplar-try

---
+++ models/marts/core/fct_orders.sql
@@ -0,0 +1,7 @@
+with orders as (
+    select * from {{ ref('stg_orders') }}
+),
+payments as (
+    select * from {{ ref('stg_payments') }}
+)
+select order_id, customer_id, amount from orders left join payments using(order_id)
